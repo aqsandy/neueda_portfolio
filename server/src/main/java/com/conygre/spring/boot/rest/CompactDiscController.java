7624d33d495f3cdce5f924f863616cd7cfffd627
wangmuy2 <aqsandy@live.com>
2022-08-12 13:00:54 -0400

    entity changed.

--- server/src/main/java/com/conygre/spring/boot/rest/CompactDiscController.java
+++ server/src/main/java/com/conygre/spring/boot/rest/CompactDiscController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.Collection;
 
 @RestController
-@RequestMapping("/api/compactdiscs")
+@RequestMapping("/api/trade")
 @CrossOrigin // allows requests from all domains
 public class CompactDiscController {
 
