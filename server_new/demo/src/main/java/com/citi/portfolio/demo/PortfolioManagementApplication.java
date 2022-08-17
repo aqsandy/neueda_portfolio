e4be363b0e86b691205950124c490c7562d934b6
Mari5498 <aryan-7@hotmail.com>
2022-08-17 16:21:58 -0400

    Added boilerplate spring boot application code to server_new

---
+++ server_new/demo/src/main/java/com/citi/portfolio/demo/PortfolioManagementApplication.java
@@ -0,0 +1,13 @@
+package com.citi.portfolio.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortfolioManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortfolioManagementApplication.class, args);
+	}
+
+}
