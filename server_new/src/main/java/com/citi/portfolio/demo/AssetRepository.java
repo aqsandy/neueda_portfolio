74a4d0ad993a80ad23e661395d44f161de43e3e1
Mari5498 <aryan-7@hotmail.com>
2022-08-18 10:59:36 -0400

    added repository and controlerr

---
+++ server_new/src/main/java/com/citi/portfolio/demo/AssetRepository.java
@@ -0,0 +1,7 @@
+package com.citi.portfolio.demo;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface AssetRepository extends CrudRepository<Asset,Integer>{
+
+}
---
+++ server_new/src/main/java/com/citi/portfolio/demo/TradeController.java
@@ -0,0 +1,21 @@
+package com.citi.portfolio.demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping(path="/trade")
+public class TradeController {
+     @Autowired
+     private AssetRepository assetRepository;
+
+    @GetMapping(path="/all")
+    public @ResponseBody Iterable<Asset> getAllAssets() {
+        // This returns a JSON or XML with the users
+        return assetRepository.findAll();
+    }
+
+}
