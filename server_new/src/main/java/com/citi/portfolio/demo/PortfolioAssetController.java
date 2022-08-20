a2d9141ea9f1657179d8f15b4a4b743661970dd5
Sandy Wang <sandywang@Sandys-Mac-mini.local>
2022-08-19 22:57:14 -0400

    Updated the portfolio api from using @responsebody to use responseentity

--- server_new/src/main/java/com/citi/portfolio/demo/PortfolioAssetController.java
+++ server_new/src/main/java/com/citi/portfolio/demo/PortfolioAssetController.java
@@ -1,5 +1,6 @@
 package com.citi.portfolio.demo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.json.*;
@@ -13,24 +14,46 @@ public class PortfolioAssetController {
     @Autowired
     private PortfolioAssetRepository portfolioAssetRepository;
 
-    @PostMapping(path = "/buy")
-    public @ResponseBody String buy_asset(@RequestParam Integer pid, @RequestParam Integer aid,
-                                         @RequestParam Float buy_price, @RequestParam Integer shares){
+    @PostMapping(path = "/add")
+    ResponseEntity<String> add_asset(@RequestParam Integer pid, @RequestParam Integer aid,
+                                                  @RequestParam Float buy_price, @RequestParam Integer shares){
         Map<String, Object> responseBody = new HashMap<String, Object>();
         PortfolioAsset asset = new PortfolioAsset();
-        asset.setPid(pid);
-        asset.setAid(aid);
-        asset.setBuy_price(buy_price);
-        asset.setShares(shares);
-        Float total_cost = buy_price * shares;
-        asset.setTotal_cost(total_cost);
+        try {
+            asset.setPid(pid);
+            asset.setAid(aid);
+            asset.setBuy_price(buy_price);
+            asset.setShares(shares);
+            Float total_cost = buy_price * shares;
+            asset.setTotal_cost(total_cost);
+            portfolioAssetRepository.save(asset);
+        }catch(Exception e) {
+            responseBody.put("status","BAD REQUEST");
+            responseBody.put("error",e.toString());
+            return ResponseEntity.badRequest().body(new JSONObject(responseBody).toString());
+        }
         responseBody.put("status","OK");
-        String responseString = (new JSONObject(responseBody)).toString(2);
-        return responseString;
+        String responseString = (new JSONObject(responseBody).toString());
+        return ResponseEntity.accepted().body(responseString);
     }
     @GetMapping(path="/all")
     public @ResponseBody Iterable<PortfolioAsset> getAllAssets() {
-        // This returns a JSON or XML with the users
         return portfolioAssetRepository.findAll();
+    }
+    @PostMapping(path = "/delete")
+    ResponseEntity<String> delete_asset(@RequestParam Integer product_id){
+        Map<String, Object> responseBody = new HashMap<String, Object>();
+        PortfolioAsset asset = new PortfolioAsset();
+        asset.setProduct_id(product_id);
+        try{
+            portfolioAssetRepository.deleteById(product_id);
+        } catch(Exception e){
+            responseBody.put("status","BAD REQUEST");
+            responseBody.put("error",e.toString());
+            return ResponseEntity.badRequest().body(new JSONObject(responseBody).toString());
+        }
+        responseBody.put("status","OK");
+        String responseString = (new JSONObject(responseBody).toString());
+        return ResponseEntity.accepted().body(responseString);
     }
 }
--- server_new/src/main/java/com/citi/portfolio/demo/PortfolioAssetRepository.java
+++ server_new/src/main/java/com/citi/portfolio/demo/PortfolioAssetRepository.java
@@ -1,7 +1,7 @@
 package com.citi.portfolio.demo;
 
 import org.springframework.data.repository.CrudRepository;
-
+import com.citi.portfolio.demo.PortfolioAsset;
 public interface PortfolioAssetRepository extends CrudRepository<PortfolioAsset, Integer>  {
 
 }
