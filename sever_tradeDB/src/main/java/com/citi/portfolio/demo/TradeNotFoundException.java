886953dba24fabf7e495c2e444bcd85ea1381a60
Mari5498 <aryan-7@hotmail.com>
2022-08-21 22:53:00 -0400

    Deleted server_tradeDB and old server

--- sever_tradeDB/src/main/java/com/citi/portfolio/demo/TradeNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package com.citi.portfolio.demo;
-
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
-
-@ResponseStatus(value = HttpStatus.NOT_FOUND, reason = "No such trade")  // 404
-public class TradeNotFoundException extends RuntimeException {
-
-}
--- sever_tradeDB/src/main/java/com/citi/portfolio/demo/TradeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.citi.portfolio.demo;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface TradeRepository extends CrudRepository<TradeOrder,Integer>{
-
-    public TradeOrder findByOrderTicker(String ticker);
-
-}
